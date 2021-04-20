c9bfbdac736f73ff7288bd0d6511b144cf1c6d08
Rafaela <rafaela.barbara.custodio@gmail.com>
2021-04-19 22:50:08 -0300

    Produtos e associação muitos para muitos

--- backend/src/main/java/com/rafaelacustodio/salesapp/SalesappApplication.java
+++ backend/src/main/java/com/rafaelacustodio/salesapp/SalesappApplication.java
@@ -1,7 +1,10 @@
 package com.rafaelacustodio.salesapp;
 
 import com.rafaelacustodio.salesapp.domain.Categoria;
+import com.rafaelacustodio.salesapp.domain.Produto;
 import com.rafaelacustodio.salesapp.repositories.CategoriaRepository;
+import com.rafaelacustodio.salesapp.repositories.ProdutoRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +18,9 @@ public class SalesappApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SalesappApplication.class, args);
 	}
@@ -24,6 +30,19 @@ public class SalesappApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 	}
 }
--- backend/src/main/java/com/rafaelacustodio/salesapp/domain/Categoria.java
+++ backend/src/main/java/com/rafaelacustodio/salesapp/domain/Categoria.java
@@ -1,9 +1,10 @@
 package com.rafaelacustodio.salesapp.domain;
+import com.rafaelacustodio.salesapp.domain.Produto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Categoria {
@@ -12,13 +13,23 @@ public class Categoria {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
+
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	public Categoria() {}
 
 	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	public Integer getId() {
@@ -38,36 +49,15 @@ public class Categoria {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		return result;
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Categoria)) return false;
+		Categoria categoria = (Categoria) o;
+		return Objects.equals(getId(), categoria.getId()) && Objects.equals(getNome(), categoria.getNome()) && Objects.equals(getProdutos(), categoria.getProdutos());
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Categoria other = (Categoria) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
-		return true;
+	public int hashCode() {
+		return Objects.hash(getId(), getNome(), getProdutos());
 	}
-	
-	
-	
 }
