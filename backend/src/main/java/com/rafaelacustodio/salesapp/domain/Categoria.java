80127dd7b7445d584d5a10c891775a4f4c708609
Rafaela <rafaela.barbara.custodio@gmail.com>
2021-04-19 21:31:32 -0300

    banco de dados h2 e criação da base de dados automatica

--- backend/src/main/java/com/rafaelacustodio/salesapp/domain/Categoria.java
+++ backend/src/main/java/com/rafaelacustodio/salesapp/domain/Categoria.java
@@ -1,6 +1,15 @@
 package com.rafaelacustodio.salesapp.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
