d66724a7ad7a4bd1a3ce4f843532a058949aef33
Rafaela <rafaela.barbara.custodio@gmail.com>
2021-04-19 23:45:44 -0300

    Cidade e Estado

--- backend/src/main/java/com/rafaelacustodio/salesapp/SalesappApplication.java
+++ backend/src/main/java/com/rafaelacustodio/salesapp/SalesappApplication.java
@@ -1,8 +1,12 @@
 package com.rafaelacustodio.salesapp;
 
 import com.rafaelacustodio.salesapp.domain.Categoria;
+import com.rafaelacustodio.salesapp.domain.Cidade;
+import com.rafaelacustodio.salesapp.domain.Estado;
 import com.rafaelacustodio.salesapp.domain.Produto;
 import com.rafaelacustodio.salesapp.repositories.CategoriaRepository;
+import com.rafaelacustodio.salesapp.repositories.CidadeRepository;
+import com.rafaelacustodio.salesapp.repositories.EstadoRepository;
 import com.rafaelacustodio.salesapp.repositories.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +24,12 @@ public class SalesappApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SalesappApplication.class, args);
@@ -43,6 +53,23 @@ public class SalesappApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "São Paulo", est2);
+		Cidade c2 = new Cidade(null, "Varginha", est1);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c1, c3));
+		
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+
 
 	}
 }
