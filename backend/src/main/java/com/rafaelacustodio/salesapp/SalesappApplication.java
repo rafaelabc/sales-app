775a325ae77e341d65501ffe8e1f4cffde20c63f
Rafaela <rafaela.barbara.custodio@gmail.com>
2021-04-19 22:15:09 -0300

    criando operação de instanciacao

--- backend/src/main/java/com/rafaelacustodio/salesapp/SalesappApplication.java
+++ backend/src/main/java/com/rafaelacustodio/salesapp/SalesappApplication.java
@@ -1,13 +1,29 @@
 package com.rafaelacustodio.salesapp;
 
+import com.rafaelacustodio.salesapp.domain.Categoria;
+import com.rafaelacustodio.salesapp.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SalesappApplication {
+public class SalesappApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalesappApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
