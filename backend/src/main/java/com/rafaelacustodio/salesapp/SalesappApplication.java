7968f5e1853c53d3311f193706c2f5ec456856f6
Rafaela <rafaela.barbara.custodio@gmail.com>
2021-04-22 23:40:33 -0300

    ItemPedido e ItemPedidoPK

--- backend/src/main/java/com/rafaelacustodio/salesapp/SalesappApplication.java
+++ backend/src/main/java/com/rafaelacustodio/salesapp/SalesappApplication.java
@@ -13,6 +13,7 @@ import com.rafaelacustodio.salesapp.domain.Cidade;
 import com.rafaelacustodio.salesapp.domain.Cliente;
 import com.rafaelacustodio.salesapp.domain.Endereco;
 import com.rafaelacustodio.salesapp.domain.Estado;
+import com.rafaelacustodio.salesapp.domain.ItemPedido;
 import com.rafaelacustodio.salesapp.domain.Pagamento;
 import com.rafaelacustodio.salesapp.domain.PagamentoComBoleto;
 import com.rafaelacustodio.salesapp.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.rafaelacustodio.salesapp.repositories.CidadeRepository;
 import com.rafaelacustodio.salesapp.repositories.ClienteRepository;
 import com.rafaelacustodio.salesapp.repositories.EnderecoRepository;
 import com.rafaelacustodio.salesapp.repositories.EstadoRepository;
+import com.rafaelacustodio.salesapp.repositories.ItemPedidoRepository;
 import com.rafaelacustodio.salesapp.repositories.PagamentoRepository;
 import com.rafaelacustodio.salesapp.repositories.PedidoRepository;
 import com.rafaelacustodio.salesapp.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class SalesappApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SalesappApplication.class, args);
@@ -124,7 +129,19 @@ public class SalesappApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 =  new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 =  new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 =  new ItemPedido(ped2, p2, 100.00, 1, 800.00);
 
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 }
--- backend/src/main/java/com/rafaelacustodio/salesapp/domain/Produto.java
+++ backend/src/main/java/com/rafaelacustodio/salesapp/domain/Produto.java
@@ -6,7 +6,9 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Produto implements Serializable {
@@ -25,13 +27,27 @@ public class Produto implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "categoria_id")
     )
     private List<Categoria> categorias = new ArrayList<>();
+    
+    @OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+
     public Produto(){}
+    
     public Produto(Integer id, String nome, Double preco) {
         this.id = id;
         this.nome = nome;
         this.preco = preco;
     }
 
+    
+    public List<Pedido> getPedidos(){
+    	List<Pedido> lista = new ArrayList<>();
+    	for(ItemPedido x : itens) {
+    		lista.add(x.getPedido());
+    	}
+    	return lista;
+    }
+    
     public Integer getId() {
         return id;
     }
@@ -63,6 +79,14 @@ public class Produto implements Serializable {
     public void setCategorias(List<Categoria> categorias) {
         this.categorias = categorias;
     }
+    
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -86,5 +110,7 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
+
+
     
 }
