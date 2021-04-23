7ee6c47850814936038fd5b3a92c15f024d04c81
Rafaela <rafaela.barbara.custodio@gmail.com>
2021-04-22 21:27:06 -0300

    Cliente, endereço, tipo cliente e telefones

--- backend/src/main/java/com/rafaelacustodio/salesapp/SalesappApplication.java
+++ backend/src/main/java/com/rafaelacustodio/salesapp/SalesappApplication.java
@@ -2,10 +2,15 @@ package com.rafaelacustodio.salesapp;
 
 import com.rafaelacustodio.salesapp.domain.Categoria;
 import com.rafaelacustodio.salesapp.domain.Cidade;
+import com.rafaelacustodio.salesapp.domain.Cliente;
+import com.rafaelacustodio.salesapp.domain.Endereco;
 import com.rafaelacustodio.salesapp.domain.Estado;
 import com.rafaelacustodio.salesapp.domain.Produto;
+import com.rafaelacustodio.salesapp.domain.enums.TipoCliente;
 import com.rafaelacustodio.salesapp.repositories.CategoriaRepository;
 import com.rafaelacustodio.salesapp.repositories.CidadeRepository;
+import com.rafaelacustodio.salesapp.repositories.ClienteRepository;
+import com.rafaelacustodio.salesapp.repositories.EnderecoRepository;
 import com.rafaelacustodio.salesapp.repositories.EstadoRepository;
 import com.rafaelacustodio.salesapp.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class SalesappApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SalesappApplication.class, args);
@@ -69,7 +80,19 @@ public class SalesappApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
+		
+		Cliente cli1 =  new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+	
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+	
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+	
 	}
 }
--- backend/src/main/java/com/rafaelacustodio/salesapp/domain/Categoria.java
+++ backend/src/main/java/com/rafaelacustodio/salesapp/domain/Categoria.java
@@ -2,12 +2,16 @@ package com.rafaelacustodio.salesapp.domain;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Categoria {
+public class Categoria implements Serializable {
 	
+	
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
