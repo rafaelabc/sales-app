e62617882de0ba546bf40ca9013eff431a9125b8
rrafaelac <rafaela.custodio.ext@vr.com.br>
2021-09-20 18:55:49 -0300

    Implementando toString de Pedido e ItemPedido

--- src/main/java/com/rafaelacustodio/salesapp/services/PedidoService.java
+++ src/main/java/com/rafaelacustodio/salesapp/services/PedidoService.java
@@ -33,6 +33,9 @@ public class PedidoService {
     @Autowired
 	private ItemPedidoRepository itemPedidoRepository;
     
+    @Autowired
+    private ClienteService clienteService;
+    
     public Pedido find(Integer id){
     	Optional<Pedido> obj = repository.findById(id);
     	return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -43,12 +46,14 @@ public class PedidoService {
     public Pedido insert(Pedido obj) {
     	obj.setId(null);
     	obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
     	obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
     	obj.getPagamento().setPedido(obj);
     	if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
+    	obj = repository.save(obj);
     	pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
@@ -57,6 +62,9 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		
+		System.out.println(obj);
+		
 		return obj;
 
 
