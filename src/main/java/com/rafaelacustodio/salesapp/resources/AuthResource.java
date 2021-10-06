4eb7fa696cb33643ed856ca0dbbbcf7342cbf7cf
rafaelabc <rafaela.barbara.custodio@gmail.com>
2021-10-06 18:14:44 -0300

    Esqueci a senha

--- src/main/java/com/rafaelacustodio/salesapp/resources/AuthResource.java
+++ src/main/java/com/rafaelacustodio/salesapp/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.rafaelacustodio.salesapp.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rafaelacustodio.salesapp.dto.EmailDTO;
 import com.rafaelacustodio.salesapp.security.JWTUtil;
 import com.rafaelacustodio.salesapp.security.UserSS;
+import com.rafaelacustodio.salesapp.services.AuthService;
 import com.rafaelacustodio.salesapp.services.UserService;
 
 @RestController
@@ -19,12 +23,21 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
+	@Autowired
+	private AuthService service;
+	
 	@RequestMapping(value = "/refresh-token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response){
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
 		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto){
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
