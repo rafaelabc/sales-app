e13a889a920d99a5e5ed6f0836831ae509b278c3
rafaelabc <rafaela.barbara.custodio@gmail.com>
2021-10-07 19:04:19 -0300

    salvando primeiro arquivo no S3

--- src/main/java/com/rafaelacustodio/salesapp/SalesappApplication.java
+++ src/main/java/com/rafaelacustodio/salesapp/SalesappApplication.java
@@ -1,13 +1,17 @@
 package com.rafaelacustodio.salesapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rafaelacustodio.salesapp.services.S3Service;
+
 @SpringBootApplication
 public class SalesappApplication implements CommandLineRunner {
 
-	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SalesappApplication.class, args);
@@ -15,7 +19,7 @@ public class SalesappApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("D:\\temp\\fotos\\marilia.jpg");
 
 	}
 }
