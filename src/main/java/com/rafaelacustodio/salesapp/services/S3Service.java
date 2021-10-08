199c2214b0ca27d4af2a0afdba806459698dae7b
rafaelabc <rafaela.barbara.custodio@gmail.com>
2021-10-08 11:44:16 -0300

    Tratamento de exceptions o upload de imagens

--- src/main/java/com/rafaelacustodio/salesapp/services/S3Service.java
+++ src/main/java/com/rafaelacustodio/salesapp/services/S3Service.java
@@ -8,11 +8,13 @@ import java.net.URISyntaxException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.rafaelacustodio.salesapp.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -20,8 +22,8 @@ public class S3Service {
 	@Autowired
 	private AmazonS3 s3client;
 
-//	@Value("${s3.bucket}")
-	private String bucketName = "sales-app-backend";
+	@Value("${s3.bucket}")
+	private String bucketName;
 
 	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
 
@@ -35,7 +37,7 @@ public class S3Service {
 			
 		} catch (IOException e) {
 			
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 		
 
@@ -52,7 +54,7 @@ public class S3Service {
 
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 }
