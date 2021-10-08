84d0ccea59353f997a4a0e3c7a75665bff1dbf73
rafaelabc <rafaela.barbara.custodio@gmail.com>
2021-10-08 18:26:28 -0300

    Padronizando formato de excepitions

--- src/main/java/com/rafaelacustodio/salesapp/resources/exception/ValidationError.java
+++ src/main/java/com/rafaelacustodio/salesapp/resources/exception/ValidationError.java
@@ -8,11 +8,13 @@ public class ValidationError extends StandardError{
     
     private List<FieldMessage> errors = new ArrayList<>();
     
-    public ValidationError(Integer status, String msg, Long timeStamp) {
-        super(status, msg, timeStamp);
-    }
+   
     
-    public List<FieldMessage> getErrors(){
+    public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
+	}
+
+	public List<FieldMessage> getErrors(){
         return errors;
     }
 
